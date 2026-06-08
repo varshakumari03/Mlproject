c85c5adbedb7e9021a480104ed78b49065ab32df
Varsha <varshaojha584@gmail.com>
2026-06-08 12:43:27 +0530

    Data Ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 
 def error_message_detail(error, error_detail: sys):
@@ -20,5 +20,11 @@ class CustomException(Exception):
         return self.error_message
 
 
-        
+if __name__=="__main__":
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Divide by zero")  
+        raise CustomException(e,sys)
+
        
